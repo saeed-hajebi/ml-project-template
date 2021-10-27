6b11953109124fba184345d9c0d3b697d64b5476
Saeed Hajebi <saeed@BRUIHADcBEqd569.levi.com>
2021-10-27 13:21:16 +0100

    Added to config.py

--- {{ cookiecutter.repo_name }}/src/config/config.py
+++ {{ cookiecutter.repo_name }}/src/config/config.py
@@ -4,22 +4,27 @@ import sys
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
 
 ## models
-TRAINED_MODEL_DIR = PROJECT_ROOT / 'models'
-DATASET_DIR = PROJECT_ROOT / 'data'
+TRAINED_MODEL_DIR = PROJECT_ROOT / "models"
+DATASET_DIR = PROJECT_ROOT / "data"
 
 ## data
-TESTING_DATA_FILE = DATASET_DIR / 'processed/train'
-TRAINING_DATA_FILE =  DATASET_DIR / 'processed/test'
-TARGET = ''
+TESTING_DATA_FILE = DATASET_DIR / "processed/train"
+TRAINING_DATA_FILE = DATASET_DIR / "processed/test"
+TARGET = ""
 
 # print(*[sys.path, PROJECT_ROOT, TESTING_DATA_FILE, TRAINING_DATA_FILE], sep='\n')
 
 ## variables
+ID_COLUMNS = []
+SNAPSHOT_DATE_COLUMN = ""
+JOIN_KEY_COLUMNS = []
+FILTER_COLUMNS = []
+
 FEATURES = []
-FEATURES_FINAL = []         # Final feature to keep in data
-FEATURES_NUMERICAL = []     # Numerical
-FEATURES_CATEGORICAL = []   # Categorical
-FEATURES_TO_ENCODE = []     # Features to Encode
-FEATURES_TEMPORAL = []      # Temporal (date/time)
-FEATURES_LOG = []           # Features for Log Transform
-FEATURES_DROP = []          # Features to Drop
+FEATURES_FINAL = []  # Final feature to keep in data
+FEATURES_NUMERICAL = []  # Numerical
+FEATURES_CATEGORICAL = []  # Categorical
+FEATURES_TO_ENCODE = []  # Features to Encode
+FEATURES_TEMPORAL = []  # Temporal (date/time)
+FEATURES_LOG = []  # Features for Log Transform
+FEATURES_DROP = []  # Features to Drop
